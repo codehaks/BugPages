66d7fd3c91bdac212dd8c9c6f9972a53bbcea1ea
Hakim Ghods <ghods.hakim@hotmail.com>
2018-05-04 22:41:14 +0430

    Details

---
+++ BugPages/Pages/Details.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BugPages.Models;
+using LiteDB;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BugPages.Pages
+{
+    public class DetailsModel : PageModel
+    {
+
+      
+        public Bug Bug { get; set; }
+
+        public void OnGet(int id)
+        {
+            using (var db = new LiteDatabase(@"bug.db"))
+            {
+                Bug = db.GetCollection<Bug>().FindById(id);
+            }
+        }
+    }
+}

cd9259f1dd6f7ac25f8187022964a7c7b2692f60
Hakim Ghods <ghods.hakim@hotmail.com>
2018-05-04 22:46:24 +0430

    Pages

---
+++ BugPages/Pages/Edit.cshtml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BugPages.Models;
+using LiteDB;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BugPages.Pages
+{
+    public class EditModel : PageModel
+    {
+        [BindProperty]
+        public Bug Bug { get; set; }
+
+        public void OnGet(int id)
+        {
+            using (var db = new LiteDatabase(@"bug.db"))
+            {
+                Bug = db.GetCollection<Bug>().FindById(id);
+            }
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            using (var db = new LiteDatabase(@"bug.db"))
+            {
+                var bugs = db.GetCollection<Bug>();
+                bugs.Update(Bug);
+
+            }
+            return RedirectToPage("./index");
+
+        }
+    }
+}

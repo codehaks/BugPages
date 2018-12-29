c15942a6bd3c87b308681a16fb35da9392893c47
Hakim Ghods <ghods.hakim@hotmail.com>
2018-12-29 16:59:53 +0330

    List

---
+++ BugPages/Controllers/TestController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BugPages.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BugPages.Controllers
+{
+    public class TestController : Controller
+    {
+        [Route("test/Index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("test/create")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [Route("test/create")]
+        [HttpPost]
+        public IActionResult Create(IList<Bug> bugs)
+        {
+            return View();
+        }
+
+    }
+}
---
+++ BugPages/Pages/Admin/Users/Index.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BugPages.Pages.Admin.Users
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+
+        public void OnPost()
+        {
+
+        }
+
+
+    }
+}
